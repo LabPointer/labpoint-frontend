41f464183b3f149fc4a13b3ba5a07a575ee3f6e9
Nathan Miguel <nathan.cm77@gmail.com>
2026-04-10 11:44:42 -0300

    fixed ip addr and utils folder structure for autoimport

--- composables/fastify.ts
+++
@@ -1,6 +0,0 @@
-import createClient from "openapi-fetch";
-import type { paths } from "~~/types";
-
-const client = createClient<paths>({ baseUrl: "http://192.168.1.3:3001/" });
-
-export const useFastify = () => client;
---
+++ shared/utils/fastify.ts
@@ -0,0 +1,13 @@
+import createClient from "openapi-fetch";
+import type { paths } from "~~/types";
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    return `${window.location.protocol}//${window.location.hostname}:3001/`;
+  }
+  return "http://localhost:3001/";
+};
+
+const client = createClient<paths>({ baseUrl: getBaseUrl() });
+
+export const useFastify = () => client;
